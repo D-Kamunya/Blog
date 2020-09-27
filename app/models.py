32749d59e8a03e0ac8967ff6a49e82718fb5f775
Dennis Kamunya <dennismuriithik@gmail.com>
2020-09-27 11:18:21 +0300

    Create user model schema class

---
+++ app/models.py
@@ -0,0 +1,17 @@
+from . import db
+from datetime import datetime
+
+class User(db.Model):
+
+    'User model schema'
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pass_secure = db.Column(db.String(255))
+   
+    
