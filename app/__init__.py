8dec4ee7dc41170e4f2c3016520ea756493b4533
Dennis Kamunya <dennismuriithik@gmail.com>
2020-09-27 11:08:07 +0300

    Initialize flask_sqlalchemy extension

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 
 def create_app(config_name):
 
@@ -8,6 +10,9 @@ def create_app(config_name):
 
   # Creating the app configurations
   app.config.from_object(config_options[config_name])
+
+  # Initializing flask extensions
+  db.init_app(app)
 
 
   # Registering the blueprints
