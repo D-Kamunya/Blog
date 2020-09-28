fbdce1cdb63dd3a0379b5e64e6b6942649cbd3f4
Dennis Kamunya <dennismuriithik@gmail.com>
2020-09-28 08:18:53 +0300

    Initialize flask uploads extension

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
-  
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 class ProdConfig(Config):
     '''
