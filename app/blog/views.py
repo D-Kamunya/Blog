49e08b15494db973027072072ec480cfa5305a1f
Dennis Kamunya <dennismuriithik@gmail.com>
2020-09-28 07:35:19 +0300

    Create navbar html template

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from . import blog
-from flask_login import login_required
+from flask_login import login_required,current_user
 
 
 # Views
@@ -12,5 +12,5 @@ def index():
     View root page function that returns the index page and its data
     '''
 
-    return render_template('index.html')
+    return render_template('index.html',user = current_user)
 
