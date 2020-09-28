50bce4dbaa7f7776a27ca7e2136fb087ec5c15ee
Dennis Kamunya <dennismuriithik@gmail.com>
2020-09-28 08:01:39 +0300

    Create updateprofile flask form

---
+++ app/blog/forms.py
@@ -0,0 +1,7 @@
+from flask_wtf import FlaskForm
+from wtforms import SelectField,StringField,TextAreaField,SubmitField
+from wtforms.validators import Required
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Edit your bio',validators = [Required()])
+    submit = SubmitField('Edit bio')
