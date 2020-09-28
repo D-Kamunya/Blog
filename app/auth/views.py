39dd182cf47dad29e0d53b92f1c89078e379afee
Dennis Kamunya <dennismuriithik@gmail.com>
2020-09-28 07:54:37 +0300

    Create auth logout view fn

--- app/auth/views.py
+++ app/auth/views.py
@@ -31,3 +31,8 @@ def register():
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)    
 
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("auth.login"))
