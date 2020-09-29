f30ce79191cd9bc3dbf4c9c30078c9a87dc5474d
Dennis Kamunya <dennismuriithik@gmail.com>
2020-09-29 14:08:35 +0300

    Add logic to send email to users on registering

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,6 +4,7 @@ from ..models import User
 from .. import db
 from flask_login import login_user,logout_user,login_required
 from .forms import LoginForm,RegistrationForm
+from ..email import mail_message
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
@@ -27,6 +28,7 @@ def register():
         db.session.add(user)
         db.session.commit()
 
+        mail_message("Welcome to My Blog","email/welcome_user",user.email,user=user)
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)    
